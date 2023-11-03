5165db10df7c41e15e6e5995c538a08c600fa572
Alexander Machado <alexuandermachado@gmail.com>
2023-11-03 11:45:54 -0600

    🛠️Configuracion de morgan personalizada

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -2,9 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import indexRoutes from '../api/routes/index.routes.js';
 import { handlRoutesErrors } from '../api/middlewares/utils/utils.js';
-import passport from 'passport';
 import morgan from 'morgan';
-import { accessLogStream } from '../config/morgan.js';
+import { accessLogStream, assignId, morganFormat } from '../config/morgan.js';
 
 export default async ({ app }) => {
   app.use(
@@ -15,18 +14,19 @@ export default async ({ app }) => {
   );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(passport.initialize());
-  morgan.token('type', (req, res) => {
-    return req.headers['content-type'];
+  // ? Morgan config
+  morgan.token('id', (req, res) => {
+    return req.id;
   });
-
-  app.use(
-    morgan(
-      ':method :url :status :res[content-length] - :response-time ms :date[web] :type',
-      { stream: accessLogStream }
-    )
-  );
+  app.use(assignId);
+  app.use(morgan(morganFormat, { stream: accessLogStream }));
+  app.use(morgan(morganFormat));
+  // ? -----------------------------------------
   app.use('/apiEcomerce/1.0', indexRoutes);
   app.use(handlRoutesErrors);
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 };
 // * http//localhost:4000/apiEcomerce/1.0/ -> indexRoutes
